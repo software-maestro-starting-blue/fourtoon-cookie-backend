0689e98e8edbe4c317b12fc59e8387b142535142
JuneBridge <junebridge03@g.skku.edu>
2024-07-30 12:37:49 +0900

    feat: StabilityAIVisionApplyService의 기본적인 뼈대 로직 작성

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/vision/apply/service/StabilityAIVisionApplyService.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/vision/apply/service/StabilityAIVisionApplyService.java
@@ -2,11 +2,16 @@ package com.startingblue.fourtooncookie.vision.apply.service;
 
 import com.startingblue.fourtooncookie.character.domain.Character;
 import com.startingblue.fourtooncookie.character.domain.CharacterVisionType;
+import com.startingblue.fourtooncookie.vision.reply.dto.VisionReplyEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Base64;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -15,20 +20,48 @@ public class StabilityAIVisionApplyService implements VisionApplyService {
 
     private final ApplicationEventPublisher applicationEventPublisher;
 
+    @Value("${stability.api.key}")
+    private String API_KEY;
+
     private final RestTemplate restTemplate = new RestTemplate();
 
 
     @Override
     public void processVisionApply(Long diaryId, List<String> contents, Character character) {
 
-        //TODO: 캐릭터에 맞는 시드를 추출
+        Integer seed = getSeed(character);
 
-        contents.forEach(content -> {
-            //TODO: content를 프롬프트로 수정
-            //TODO: 프롬프트를 활용하여 requestBody 구성
-            //TODO: requestBody를 이용하여 AI 서버에 요청
-            //TODO: 응답을 받아서 event Call
-        });
+        for (int i = 0; i < 4; i++) {
+            String content = contents.get(i);
+            String prompt = generatePrompt(content, character);
+            String imageB64Json = getImageOfBase64DataFromStabilityAI(prompt, seed);
+            byte[] imageOfBytes = Base64.getDecoder().decode(imageB64Json);
+            applicationEventPublisher.publishEvent(new VisionReplyEvent(diaryId, imageOfBytes, i));
+        }
+    }
+
+    private Integer getSeed(Character character) {
+        return null; //TODO: 캐릭터에 맞는 시드 추출
+    }
+
+    private String generatePrompt(String content, Character character) {
+        return null; //TODO: 캐릭터에 맞는 프롬프트 생성
+    }
+
+    private String getImageOfBase64DataFromStabilityAI(String prompt, Integer seed) {
+        return null; //TODO: AI 서버에 요청하여 이미지(base64) 받아오기
+    }
+
+    private HttpHeaders getHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + API_KEY);
+        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        return headers;
+    }
+
+    private String getRequestBody(String prompt, Integer seed) {
+        return null; //TODO: 프롬프트를 이용하여 requestBody 생성
     }
 
     @Override
